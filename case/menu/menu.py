aca4c833f35fdc81a650487f31cfb841481164f3
pororo421 <215028697@qq.com>
2020-04-12 17:43:45 +0900

    三级菜单完整版

--- case/menu/menu.py
+++ case/menu/menu.py
@@ -21,14 +21,55 @@ china_city = {
         "鸡西市": ['鸡冠区', '城子河区', '恒山区']
     }
 }
+exit_menu = "0"
+return_menu = "1"
+main_menu = "2"
+is_start = True  # 想要停止是False
+is_city = True  # 想要停止是False
+is_area = True
 
-for key in china_city:
-    print(key)
-province = input("请出入您要选择的省份：")
-print("city:", china_city[province])
-for city in china_city[province]:
-    print(city)
-city = input("请输入您要选择的城市：")
-print("area:", china_city[province][city])
-for area in china_city[province][city]:
-    print(area)
+while is_start:
+    for key in china_city:
+        print(key)
+    province = input("请出入您要选择的省份（退出请输入0）：")
+
+    if province == exit_menu:
+        break
+    elif china_city.get(province, "") == "":
+        print("您输入的省份不存在！")
+    else:
+        # print("您输入的是：" + str(china_city[province]))
+        is_city = True
+        while is_city:
+            for city in china_city[province]:
+                print(city)
+                # is_start = False
+            input_city = input("请输入您要选择的城市（退出请输入0，返回请输入1）：")
+            if input_city == exit_menu:
+                break
+            elif input_city == return_menu:
+                is_city = False
+                break
+            elif china_city[province].get(input_city, "") == "":
+                print("您输入的城市不存在！")
+            else:
+                is_area = True
+                while is_area:
+                    for area in china_city[province][input_city]:
+                        print(area)
+                    input_area = input("退出请输入0，返回上一级请输入1，返回主菜单请输入2：")
+                    if input_area == exit_menu:
+                        is_area = False
+                        is_city = False
+                        is_start = False
+                        break
+                    elif input_area == return_menu:
+                        is_area = False
+                        break
+                    elif input_area == main_menu:
+                        is_area = False
+                        is_city = False
+                        break
+                    else:
+                        print("您输入有误，请重新输入！")
+                        continue
